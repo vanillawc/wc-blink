89e4830798d7bec10ee11c1206d70f5040e3f50e
Evan Plaice <evanplaice@gmail.com>
2019-09-19 14:44:21 -0600

    Add implementation

---
+++ src/wc-blink.js
@@ -0,0 +1,44 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+.blink {
+  -webkit-animation: 2s linear infinite condemned_blink_effect; // for Safari 4.0 - 8.0
+  animation: 2s linear infinite condemned_blink_effect;
+}
+@-webkit-keyframes condemned_blink_effect { // for Safari 4.0 - 8.0
+  0% {
+    visibility: hidden;
+  }
+  50% {
+    visibility: hidden;
+  }
+  100% {
+    visibility: visible;
+  }
+}
+@keyframes condemned_blink_effect {
+  0% {
+    visibility: hidden;
+  }
+  50% {
+    visibility: hidden;
+  }
+  100% {
+    visibility: visible;
+  }
+}
+</style>
+<p class="blink" style="width: inherit;"><span><slot></slot></span></p>
+`;
+
+export class WCBlink extends HTMLElement {
+
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(document.importNode(template.content, true));
+  };
+
+}
+
+customElements.define('wc-blink', WCBlink);
