cc9fb8e22afe09b449534d1c755ea1192b1e981b
Evan Plaice <evanplaice@gmail.com>
2019-12-19 19:41:16 -0700

    Refactor

--- src/wc-blink.js
+++ src/wc-blink.js
@@ -1,42 +1,45 @@
 /* eslint no-undef: 0 */
-const template = document.createElement('template');
-template.innerHTML = `
-<style>
-.blink {
-  -webkit-animation: 2s linear infinite condemned_blink_effect; // for Safari 4.0 - 8.0
-  animation: 2s linear infinite condemned_blink_effect;
-}
-@-webkit-keyframes condemned_blink_effect { // for Safari 4.0 - 8.0
-  0% {
-    visibility: hidden;
-  }
-  50% {
-    visibility: hidden;
-  }
-  100% {
-    visibility: visible;
-  }
-}
-@keyframes condemned_blink_effect {
-  0% {
-    visibility: hidden;
-  }
-  50% {
-    visibility: hidden;
-  }
-  100% {
-    visibility: visible;
-  }
-}
-</style>
-<p class="blink" style="width: inherit;"><span><slot></slot></span></p>
-`;
-
 export class WCBlink extends HTMLElement {
   constructor () {
     super();
+    const template = document.createElement('template');
+    template.innerHTML = WCBlink.default();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(document.importNode(template.content, true));
+  }
+
+  static default () {
+    return `
+      <style>
+      .blink {
+        -webkit-animation: 2s linear infinite condemned_blink_effect; // for Safari 4.0 - 8.0
+        animation: 2s linear infinite condemned_blink_effect;
+      }
+      @-webkit-keyframes condemned_blink_effect { // for Safari 4.0 - 8.0
+        0% {
+          visibility: hidden;
+        }
+        50% {
+          visibility: hidden;
+        }
+        100% {
+          visibility: visible;
+        }
+      }
+      @keyframes condemned_blink_effect {
+        0% {
+          visibility: hidden;
+        }
+        50% {
+          visibility: hidden;
+        }
+        100% {
+          visibility: visible;
+        }
+      }
+      </style>
+      <p class="blink" style="width: inherit;"><span><slot></slot></span></p>
+    `;
   }
 }
 
