f1a96bb4f72ed3b0ada6a9e6570adb4e436a4da7
Evan Plaice <evanplaice@gmail.com>
2019-12-22 14:54:04 -0700

    Maintenance

--- src/wc-blink.js
+++ src/wc-blink.js
@@ -3,28 +3,16 @@ export class WCBlink extends HTMLElement {
   constructor () {
     super();
     const template = document.createElement('template');
-    template.innerHTML = WCBlink.default();
+    template.innerHTML = WCBlink.template();
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.appendChild(document.importNode(template.content, true));
   }
 
-  static default () {
+  static template () {
     return `
       <style>
       .blink {
-        -webkit-animation: 2s linear infinite condemned_blink_effect; // for Safari 4.0 - 8.0
         animation: 2s linear infinite condemned_blink_effect;
-      }
-      @-webkit-keyframes condemned_blink_effect { // for Safari 4.0 - 8.0
-        0% {
-          visibility: hidden;
-        }
-        50% {
-          visibility: hidden;
-        }
-        100% {
-          visibility: visible;
-        }
       }
       @keyframes condemned_blink_effect {
         0% {
