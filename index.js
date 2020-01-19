e3fb1874c5ed020ca0069cd3e5cc8a64b61f38c2
Evan Plaice <evanplaice@gmail.com>
2020-01-18 19:39:54 -0700

    Add Builds

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-export * from './src/wc-blink.js';
+/* eslint no-undef: 0 */
+class WCBlink extends HTMLElement {
+  constructor () {
+    super();
+    const template = document.createElement('template');
+    template.innerHTML = WCBlink.template();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(document.importNode(template.content, true));
+  }
+
+  static template () {
+    return `
+      <style>
+      .blink {
+        animation: 2s linear infinite condemned_blink_effect;
+      }
+      @keyframes condemned_blink_effect {
+        0% {
+          visibility: hidden;
+        }
+        50% {
+          visibility: hidden;
+        }
+        100% {
+          visibility: visible;
+        }
+      }
+      </style>
+      <p class="blink" style="width: inherit;"><span><slot></slot></span></p>
+    `;
+  }
+}
+
+customElements.define('wc-blink', WCBlink);
+
+export { WCBlink };
